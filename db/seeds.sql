1b0241e43bf658653f25957a4a9ca2f4a0987eb5
Yensey Murillo <yenseymurillo@gmail.com>
2020-03-11 20:34:04 -0500

    added config and db but still need to work on the config logic

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Nacho Burger');
